eb780cd7d8c98f9fcc5f4ab0711c2c5c1340dfe5
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 10:02:37 +0400

    Added Sign up & login view function

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import CreateView
+from . import forms
 
 # Create your views here.
+
+
+class SignUp(CreateView):
+    '''
+    This will initialize a user sign up form using the create view class
+    inherited from django's class based generic views
+    '''
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/signup.html'
