99add83e01c4dc2e09b74428a9f05b7fd655736a
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 15:09:12 +0400

    User post url Added

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,7 @@ app_name='posts'
 urlpatterns = [
     url(r"^$", views.PostList.as_view(), name="all"),
     url(r"new/$", views.CreatePost.as_view(), name="create"),
+    url(r"by/(?P<username>[-\w]+)/$",views.UserPosts.as_view(),name="for_user"),
 
 
 ]
