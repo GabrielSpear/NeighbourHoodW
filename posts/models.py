f68c49debc278bd2adda9b673dbeeac889bff8a2
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 12:40:58 +0400

    Posted post to be viewed function added

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,25 @@
-from django.db import models
+rom django.db import models
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from groups.models import Group
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
-# Create your models here.
+
+class Post(models.Model):
+    '''
+    This will initialize post model which will be instiantiated
+    whenever a user creates a post
+    '''
+    user = models.ForeignKey(User, related_name='posts')
+    created_at = models.DateTimeField(auto_now=True)
+    business_name = models.TextField()
+    business_name_html = models.TextField(editable=False)
+    group = models.ForeignKey(Group, related_name='posts', null=True, blank=True)
+
+    def __str__(self):
+        '''
+        This will ensure that each post is displayed based on the message
+        in the admin panel
+        '''
+        return self.message
