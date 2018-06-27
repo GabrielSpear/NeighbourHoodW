137446ef740a0ee0608d7d9d0d90cd26a993c535
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 10:05:28 +0400

    Modes

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib import auth
 # Create your models here.
+
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+    '''
+    This will initialize the user class which inherits classes
+    from the django.contrib class
+    '''
+
+    def __str__(self):
+        '''
+        This is a string reperesentation of each user
+        '''
+        return "@{}".format(self.username)
