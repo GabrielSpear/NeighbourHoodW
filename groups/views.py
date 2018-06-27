27b36628092bf08fd019c21a3e51da8bda9f5534
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 12:31:23 +0400

    Importation of different modules in views.py

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.contrib.auth.mixins import(
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
+from django.views import generic
+from groups.models import Group, GroupMember
+from . import models
