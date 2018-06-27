ec03ae21079169ecb0169b139c1f9456d187970a
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 12:32:21 +0400

    Create new hood class added

--- groups/views.py
+++ groups/views.py
@@ -10,3 +10,26 @@ from django.db import IntegrityError
 from django.views import generic
 from groups.models import Group, GroupMember
 from . import models
+
+# Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ("name", "location")
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
+    context_object_name = 'all_groups'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Group.objects.all()
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(name__icontains=query)
+        return qs
