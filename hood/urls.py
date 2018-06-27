04a9672c684f4fe2d4654f94ca51296d35ce8965
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 16:10:12 +0400

    template doesn't exist bug cleared

--- hood/urls.py
+++ hood/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'accounts'
+app_name = 'hood'
 
 urlpatterns =[
     url(r'login/$',
-    auth_views.LoginView.as_view(template_name='accounts/login.html'),
+    auth_views.LoginView.as_view(template_name='hood/login.html'),
     name='login'),
     url(r'logout/$',auth_views.LogoutView.as_view(),name='logout'),
     url(r'signup/$',views.SignUp.as_view(),name='signup'),
