d07727bc810f200b92358020752ee0e4dea6964f
Gabriel Spear <gabrieldvjspear@gmail.com>
2018-06-27 10:23:09 +0400

    Hood group app admin panel added

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.contrib import admin
+from . import models
+# Register your models here.
 
-# Create your models here.
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+admin.site.register(models.Group)
